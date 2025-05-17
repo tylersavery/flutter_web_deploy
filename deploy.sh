80e02e136bf36902748cbed3239a83e0bf7e9521
Tyler Savery <tyler@theyoungastronauts.com>
2025-05-17 12:31:42 -0400

    part 2

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 export RELEASE="0.0.1"
+export BUCKET_NAME="flutter.lanesavery.com"
 
+export AWS_ACCESS_KEY_ID="****"
+export AWS_SECRET_ACCESS_KEY="****"
 
 rm -rf ./build/web
 
@@ -11,3 +14,14 @@ mv build/web_output/$RELEASE/index.html build/web_output/index.html
 cp build/web_output/$RELEASE/version.json build/web_output/version.json
 
 
+aws s3 cp build/web_output/$RELEASE/ s3://$BUCKET_NAME/$RELEASE/ \
+  --recursive \
+  --cache-control "public, max-age=31536000, immutable"
+
+# Upload index.html
+aws s3 cp build/web_output/index.html s3://$BUCKET_NAME/index.html \
+  --cache-control "no-store, no-cache, must-revalidate"
+
+# Upload version.json
+aws s3 cp build/web_output/version.json s3://$BUCKET_NAME/version.json \
+  --cache-control "no-store, no-cache, must-revalidate"
