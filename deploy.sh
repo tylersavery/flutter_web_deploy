8184110cb327fcc6300975970733d56a34db7337
Tyler Savery <tyler@theyoungastronauts.com>
2025-05-17 13:07:23 -0400

    part 2 with modifications

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-export RELEASE="0.0.1"
+export RELEASE="0.0.5"
 export BUCKET_NAME="flutter.lanesavery.com"
 
 export AWS_ACCESS_KEY_ID="****"
 export AWS_SECRET_ACCESS_KEY="****"
 
-rm -rf ./build/web
+rm -rf ./build
 
 flutter build web --release --base-href /$RELEASE/
 
 mkdir -p build/web_output/$RELEASE
-mv build/web/* build/web_output/$RELEASE
+mv build/web/* build/web_output/$RELEASE/
 mv build/web_output/$RELEASE/index.html build/web_output/index.html
 cp build/web_output/$RELEASE/version.json build/web_output/version.json
 
@@ -18,10 +18,8 @@ aws s3 cp build/web_output/$RELEASE/ s3://$BUCKET_NAME/$RELEASE/ \
   --recursive \
   --cache-control "public, max-age=31536000, immutable"
 
-# Upload index.html
 aws s3 cp build/web_output/index.html s3://$BUCKET_NAME/index.html \
   --cache-control "no-store, no-cache, must-revalidate"
 
-# Upload version.json
 aws s3 cp build/web_output/version.json s3://$BUCKET_NAME/version.json \
   --cache-control "no-store, no-cache, must-revalidate"
--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -105,10 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
       ),
